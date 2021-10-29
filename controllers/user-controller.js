6eb00ca15f1192eff57da8b742fd1ec96174b01f
mrralphpierre <mrralphpierre@gmail.com>
2021-10-29 19:28:27 -0400

    completed user-controller file

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,63 +1,109 @@
-const { Pizza } = require('../models');
+const { User, Thought } = require('../models');
 
-const pizzaController = {
-  // get all pizzas
-  getAllPizza(req, res) {
-    Pizza.find({})
+const UserController = {
+  // get all thoughts
+  getAllUser(req, res) {
+    User.find({})
+    .select('-__v')
+    .sort({ _id: -1 })
+    .then(dbUserData => res.json(dbUserData))
+    .catch(err => {
+      console.log(err);
+      res.sendStatus(400);
+    });
+},
+
+  // get one thought by id
+  getUserById({ params }, res) {
+    User.findOne({ _id: params.id })
       .populate({
-        path: 'comments',
+        path: 'thoughts',
+        select: '-__v'
+      })
+      .populate({
+        path: 'friends',
         select: '-__v'
       })
       .select('-__v')
-      .sort({ _id: -1 })
-      .then(dbPizzaData => res.json(dbPizzaData))
+      .then(dbUserData => res.json(dbUserData))
       .catch(err => {
         console.log(err);
         res.sendStatus(400);
       });
   },
 
-  // get one pizza by id
-  getPizzaById({ params }, res) {
-    Pizza.findOne({ _id: params.id })
-      .populate({
-        path: 'comments',
-        select: '-__v'
-      })
-      .select('-__v')
-      .then(dbPizzaData => res.json(dbPizzaData))
-      .catch(err => {
-        console.log(err);
-        res.sendStatus(400);
-      });
-  },
-
-  // createPizza
-  createPizza({ body }, res) {
-    Pizza.create(body)
-      .then(dbPizzaData => res.json(dbPizzaData))
+  // add thought to user
+  createUser({ params, body }, res) {
+    User.create(body)
+            .then(dbUserData => {
+                console.log(dbUserData);
+                res.json(dbUserData);
+            })
       .catch(err => res.json(err));
   },
 
-  // update pizza by id
-  updatePizza({ params, body }, res) {
-    Pizza.findOneAndUpdate({ _id: params.id }, body, { new: true, runValidators: true })
-      .then(dbPizzaData => {
-        if (!dbPizzaData) {
-          res.status(404).json({ message: 'No pizza found with this id!' });
+  // update Thought by id
+  addFriend({ params, body }, res) {
+    User.findOneAndUpdate(
+      { _id: params.id }, 
+      {$push: {friends: params.friendId} },
+      { new: true, runValidators: true }
+      )
+      .then(dbFriendData => {
+        if (!dbFriendData) {
+          res.status(404).json({ message: 'No user found with this id!' });
           return;
         }
-        res.json(dbPizzaData);
+        res.json(dbFriendData);
       })
       .catch(err => res.json(err));
   },
 
-  // delete pizza
-  deletePizza({ params }, res) {
-    Pizza.findOneAndDelete({ _id: params.id })
-      .then(dbPizzaData => res.json(dbPizzaData))
+  // update Thought
+  updateUser({ params, body }, res) {
+    User.findOneAndUpdate(
+        { _id: params.userId },
+        { $set: body },
+        { new: true, runValidators: true }
+    )
+        .then(dbUserData => {
+            if (!dbUserData) {
+                res.status(404).json({ message: 'No user found with this id!' });
+                return;
+            }
+            res.json(dbUserData);
+        })
+        .catch(err => res.json(err));
+},
+ // remove Thought
+ removeUser({ params }, res) {
+  User.findOneAndDelete({ _id: params.userId })
+      .then(deletedUser => {
+          if (!deletedUser) {
+              return res.status(404).json({ message: 'No user with this id!' });
+          }
+          return Thought.deleteMany({ _id: { $in: deletedUser.thoughts } })
+      }).then(() => {
+          res.json({ message: "User and associated thoughts have been deleted!" })
+      })
       .catch(err => res.json(err));
-  }
+},
+ // update reply
+ removeFriend({ params }, res) {
+  User.findOneAndUpdate(
+    { _id: params.userId },
+    {$pull: {friends: { friendId: params.friendId } } },
+      { new: true }
+    )
+    .then(dbUserData => {
+      if (!dbUserData) {
+          return res.status(404).json({ message: 'No user with this id!' });
+      }
+      res.json(dbUserData)
+  })
+  .catch(err => res.json(err));
+}
 };
 
-module.exports = pizzaController;
+
+module.exports = UserController;
